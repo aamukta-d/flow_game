8cbec25520bcd2febbc71d7d870650633d97f0fa
Alif Nafili <alifnafili@gmail.com>
2024-07-20 09:44:10 +0400

    timer fixes

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import {loadWords, checkWordNow, word_list, letterElement, randomLetter} from ".
 import {render, remove, create, addClass, hasClass, remClass, find, findAll, write, detect, undetect, style, attribs, isElement} from "./scripts/qol"
 import { real_time_left, timer, startTimer } from "./scripts/timer";
 
-
+let loop = ""
 
 async function start(){
     await loadWords()
     startTimer()
-    let loop = setInterval(game_loop, 2000);
+    if (loop === ""){
+        loop = setInterval(game_loop, 2000);
+    }
     if (real_time_left <= 1){
         clearInterval(loop)
     }
